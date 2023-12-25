85c330edc87719f633c71150dc96b07f38c7a746
Ar.rst <141205049+AlirezaRostami110@users.noreply.github.com>
2023-12-25 15:15:56 +0330

    Update testwebserver.py

--- test/testwebserver.py
+++ test/testwebserver.py
@@ -3,9 +3,9 @@ import requests
 def compare_websites(urls):
     responses = [requests.get(url).text for url in urls]
     if len(set(responses)) == 1:
-        print("The websites have the same HTML output.")
+        print("The webservers have the same HTML output.")
     else:
-        print("The websites do not have the same output.")
+        print("The webservers do not have the same output.")
 
 compare_websites(['http://0.0.0.0:8000/', 'http://0.0.0.0:5000/', 'http://0.0.0.0:7000'])
 
@@ -13,6 +13,6 @@ compare_websites(['http://0.0.0.0:8000/', 'http://0.0.0.0:5000/', 'http://0.0.0.
 def header(urls):
     for url in urls:
         response = requests.get(url)
-        print(response.headers['server'])
+        print(response.headers)
 
 header(['http://0.0.0.0:8000/', 'http://0.0.0.0:5000/', 'http://0.0.0.0:7000'])
